e8904cd88ce965588a3acfdc746ffcbe787ad5e7
Tom Westerhout <t.westerhout@student.ru.nl>
2018-08-03 18:05:03 +0100

    Made the code slightly prettier

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,22 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE FunctionalDependencies #-}
--- {-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
--- {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE ScopedTypeVariables #-}
--- {-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE LambdaCase #-}
--- {-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeInType #-}
--- {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
--- {-# LANGUAGE LiberalTypeSynonyms #-}
--- {-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE MultiWayIf #-}
--- {-# LANGUAGE UnicodeSyntax #-}
-{-# LANGUAGE CPP #-}
 
 -- |
 -- Module      : Main
@@ -40,111 +27,25 @@
 -- Stability   : experimental
 module Main where
 
-import Prelude hiding (Either(..))
+import           Prelude                 hiding ( Either(..) )
 
-import Control.Monad.Trans.State.Strict
-import Data.Constraint
-import Data.Default.Class
-import Data.Kind
-import Data.Proxy
-import Debug.Trace
-import GHC.Generics
-import GHC.TypeLits
+import           Control.Monad.Trans.State.Strict
+import           Data.Constraint
+import           Data.Default.Class
+import           Data.Kind
+import           Debug.Trace
+import           GHC.Generics
+import           Data.Vinyl                     ( ElField(..)
+                                                , Rec((:&))
+                                                )
+import qualified Data.Vinyl                    as Vinyl
 
-import Data.Vinyl (FieldRec, ElField(..), Rec(..), RecElem(..))
-import Data.Vinyl.TypeLevel (RIndex, NatToInt)
-import qualified Data.Vinyl as Vinyl
--- import qualified Data.Vinyl.ARec as Vinyl
-
-import qualified Zipper as Internal
-import Zipper (Zipper(..), Memo(..), Dissectible(..), aget, aput)
-
-
-pattern Zipper :: () => (c hole, Dissectible c hole) => hole -> Zipper c attributes root
-pattern Zipper h <- (Internal.Z h _ _)
-
-instance Default (Memo Attributes) where
-  def = M { _mCurrent = Vinyl.toARec $ Field Nothing :& Field Nothing :& Vinyl.RNil
-          , _mLeft = def
-          , _mRight = def
-          , _mUp = def
-          , _mDown = def
-          }
-
-memo ::
-     forall (name :: Symbol)
-            (field :: (Symbol, Type))
-            (attributes :: [(Symbol, Type)])
-            (x :: Type)
-            c
-            root.
-     ( KnownSymbol name
-     , NatToInt (RIndex field (Internal.ToMaybe attributes))
-     , field ~ '(name, Maybe x)
-     )
-  => State (Zipper c attributes root) x
-  -> State (Zipper c attributes root) x
-memo semantic = (aget @name <$> get) >>= \case
-  Nothing ->
-    do -- NOTE: The following line is for debugging purposes only and should not
-       -- appear in production code
-       trace ("computing " <> symbolVal (Proxy @name)) (return ())
-       y <- semantic
-       modify (aput @name y)
-       return y
-  (Just y) -> return y
+import Zipper
 
 class (a c, b c) => (&&&) (a :: Type -> Constraint) (b :: Type -> Constraint) c
 infixl 4 &&&
 
 instance (a c, b c) => (a &&& b) c
-
-checkInvalidMove :: Maybe a -> a
-checkInvalidMove (Just x) = x
-checkInvalidMove Nothing  = error $! "Invalid move attempted!"
-
-unsafeUp = checkInvalidMove . Internal.up
-unsafeDown = checkInvalidMove . Internal.down
-unsafeLeft = checkInvalidMove . Internal.left
-unsafeRight = checkInvalidMove . Internal.right
-unsafeChild i = checkInvalidMove . Internal.child i
-
-data Direction = Left
-               | Right
-               | Parent
-               | Child {-# UNPACK #-}!Int
-  deriving (Show)
-
-class WithRollback z d where
-  unsafeAt :: Show a => d -> State z a -> State z a
-
-instance WithRollback (Zipper Cxt attributes root) Direction where
-  unsafeAt direction action = case direction of
-    Left -> go unsafeLeft action unsafeRight
-    Right -> go unsafeRight action unsafeLeft
-    Parent -> do n <- Internal.whichChild <$> get
-                 modify unsafeUp
-                 x <- action
-                 modify (unsafeChild n)
-                 return x
-    Child i -> go (unsafeChild i) action unsafeUp
-    where
-      -- go :: (Zipper Cxt attributes root -> Zipper Cxt attributes root)
-      --    -> State (Zipper Cxt attributes root) a
-      --    -> (Zipper Cxt attributes root -> Zipper Cxt attributes root)
-      --    -> State (Zipper Cxt attributes root) a
-      go before now after = do
-        modify before
-        x <- now
-        modify after
-        return x
-
-
-isRoot :: State (Zipper Cxt Attributes Tree) Bool
-isRoot = get >>= \case
-  (Internal.Z _ _ (_ Internal.:>_)) -> return False
-  (Internal.Z _ _ Internal.RootContext)      -> return True
-
 
 -- User code
 --------------------------------------------------------------------------------
@@ -155,16 +56,17 @@ data Tree = Fork Tree Tree
           | Leaf Int
   deriving (Show, Read, Generic)
 
+-- | Specifies which attributes to cache
 type Attributes = '[ '("globmin", Int)
                    , '("replace", Tree)
                    ]
 
+-- | Specifies additional constraints on values in the zipper
 type Cxt = WhereAmI Position &&& Show
 
 -- | Definition of the "globmin" attribute.
---
 globmin :: State (Zipper Cxt Attributes Tree) Int
-globmin = memo @"globmin" $
+globmin = memoize @"globmin" $
   -- NOTE: The following line is for debugging purposes only and should not
   -- appear in production code
   get >>= (\(Zipper h) -> trace ("globmin on " <> show h) (return ())) >>
@@ -187,7 +89,7 @@ locmin z@(Zipper hole) =
 -- | Definition of the "replace" attribute (I'm not sure one can actually call
 -- it an attribute though).
 replace :: State (Zipper Cxt Attributes Tree) Tree
-replace = memo @"replace" $
+replace = memoize @"replace" $
   get >>= (\(Zipper h) -> trace ("globmin on " <> show h) (return ())) >>
     ( withPosition $ \case
         C_Leaf -> Leaf <$> globmin
@@ -199,11 +101,19 @@ replace = memo @"replace" $
 -- Library magic
 --------------------------------------------------------------------------------
 
+instance Default (Memo Attributes) where
+  def = M { _mCurrent = Vinyl.toARec $ Field Nothing :& Field Nothing :& Vinyl.RNil
+          , _mLeft = def
+          , _mRight = def
+          , _mUp = def
+          , _mDown = def
+          }
+
 -- | This should definitely be generated using TH or GHC.Generics. I'd prefer
 -- the latter, but I'm not sure it's possible.
 instance c Tree => Dissectible c Tree where
-  dissect (Fork l r) = Internal.LOne Fork `Internal.LCons` l `Internal.LCons` r
-  dissect x          = Internal.LOne x
+  dissect (Fork l r) = LOne Fork `LCons` l `LCons` r
+  dissect x          = LOne x
 
 -- | Represents the position in our forest of data structures.
 data Position :: Type -> Type where
@@ -236,5 +146,5 @@ t1 = Fork (Fork (Leaf 123)
           (Leaf 5)
 
 main :: IO ()
-main = print $ fst $ runState replace (Internal.enter t1)
-  -- print $ fmap showHole $ (Internal.child 0 >=> Internal.right) $ Internal.enter t1
+main = print $ fst $ runState replace (enter t1)
+  -- print $ fmap showHole $ (child 0 >=> right) $ enter t1
--- src/Zipper.hs
+++ src/Zipper.hs
@@ -1,4 +1,6 @@
 -- {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -12,6 +14,7 @@
 -- {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 -- {-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RankNTypes #-}
@@ -20,6 +23,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 -- {-# LANGUAGE LiberalTypeSynonyms #-}
@@ -36,40 +40,52 @@
 -- Maintainer  : t.westerhout@student.ru.nl
 -- Stability   : experimental
 module Zipper
-  ( -- * Core types
+  ( -- ** The Zipper
     Zipper(..)
-  , Memo(..)
-  , Context(..)
-  , LocalContext(..)
-  , Left(..)
-  , Right(..)
-  , Dissectible(..)
-  , ToMaybe
-    -- * Entering and leaving
-  , enter
-  , leave
-    -- * Movement
+  , pattern Zipper
   , up
   , down
   , left
   , right
   , child
   , whichChild
-   -- * Working with the Memo table
+  , isRoot
+  , unsafeUp
+  , unsafeDown
+  , unsafeLeft
+  , unsafeRight
+  , unsafeChild
+  , Direction(..)
+  , unsafeAt
+    -- * Entering and leaving
+  , enter
+  , leave
+    -- * Movement
+    -- * Working with the Memo table
+  , Memo(..)
+  , memoize
+    -- * Implementation details
+  , Context(..)
+  , LocalContext(..)
+  , Left(..)
+  , Right(..)
+  , Dissectible(..)
+  , ToMaybe
   , aget
   , aput
   ) where
 
--- import Data.Coerce
+import           Prelude                 hiding ( Either(..) )
+import           Control.DeepSeq
 import           Control.Monad                  ( (>=>) )
+import           Control.Monad.Trans.State.Strict
 import           Data.Default.Class
 import           Data.Constraint                ( Constraint )
 import           Data.Kind
+import           Data.Proxy
+import           Debug.Trace
 import           GHC.TypeLits
--- import           Data.Maybe                     ( fromJust )
--- import Data.Proxy
--- import GHC.Generics
--- import Unsafe.Coerce
+import           GHC.Generics                   ( Generic )
 
 import           Data.Vinyl              hiding ( RNil )
 import qualified Data.Vinyl.ARec               as Vinyl
@@ -78,66 +94,178 @@ import           Data.Vinyl.TypeLevel           ( RIndex
                                                 )
 -- import           Data.Vinyl.Functor             ( Thunk )
 
--- | The AG zipper. It consists of two zippers: @_zHole@ and @_zCxt@ constitute
--- a generic zipper into a user-defined data structure (see the Scrap Your
--- Zippers paper for more info); @_zMemo@ is a zipper into an infinite cache.
+-- | The AG zipper.
 --
--- @c@ specifies additional constraints all elements of the user-defined data
--- structures satisfy.
+-- It consists of two zippers
 --
--- @attributes@ specifies the memoized attributes.
+--   (1) A zipper into the forest of user-defined data structures.
+--   (2) A zipper into an infinite graph which is used to store computed
+--       attributes.
 --
--- @root@ specifies the type of the value for which the zipper was actually
--- created.
-data Zipper (c :: Type -> Constraint)
-            (attributes :: [(Symbol, Type)])
-            (root :: Type) =
+-- Implementation of the first zipper is very similar to the one described in
+-- ["Scrap Your Zippers: A Generic Zipper for Heterogeneous
+-- Types"](https://michaeldadams.org/papers/scrap_your_zippers/). The only
+-- difference is that we rely on the 'Dissectible' type class instead of 'Data'
+-- which allows us to propagate down arbitrary user-defined constraints @c@.
+-- For example, if our data structure is 'Show'able:
+--
+-- >>> data Tree = Fork Tree Tree | Leaf Int deriving (Show)
+-- >>> deriveDissectible ''Tree -- (not yet implemented)
+-- >>> let tree = Fork (Fork (Leaf 123) (Leaf 0)) (Leaf 5)
+-- >>> type TreeZ = Zipper Show '[] Tree
+-- >>> let showHole :: TreeZ -> String
+-- >>>     showHole (Zipper h) = show h
+-- >>> print $ showHole <$> (down >=> left >=> down >=> left >=> right) (enter tree :: TreeZ)
+-- Just "Leaf 0"
+--
+-- I don't think there is a clean way to achieve something like this using the
+-- generic zipper from the Scrap Your Zippers paper.
+--
+-- Implementation of the second zipper is quite straightforward: have a look at
+-- the 'Memo' data structure for details. The important part is that
+-- @attributes@ type allows us to specify which attributes to memoize. Each
+-- element in @attributes@ is a promoted tuple of a 'Symbol' describing the name
+-- and a 'Type' indicating the type of the attribute.
+--
+-- Finally, @root@ indicates the root of the tree we want to traverse.
+data Zipper (c :: Type -> Constraint) (attributes :: [(Symbol, Type)]) (root :: Type) =
   forall hole. (c hole, Dissectible c hole) =>
-    Z { _zHole :: hole
-      , _zMemo :: !(Memo attributes)
-      , _zCxt  :: !(Context c hole root)
+    Z { _zHole :: !hole                  -- ^ Value under cursor
+      , _zMemo :: !(Memo attributes)     -- ^ Attributes cache
+      , _zCxt  :: !(Context c hole root) -- ^ Surrounding context.
       }
 
+-- | You, as an end-user, don't care about the attribute cache and context. This
+-- pattern synonym hides this details while still providing access to the hole.
+pattern Zipper :: () => (c hole, Dissectible c hole) => hole -> Zipper c attributes root
+pattern Zipper h <- (Z h _ _)
+{-# COMPLETE Zipper #-}
+
+-- | Given a list associating names of attributes with their types,
+-- returns a list associating names of attributes with 'Maybe' their types to
+-- account for the fact that some attributes may not yet be known.
+--
+-- For example:
+--
+-- >>> :kind! ToMaybe '[ '("foo", Int), '("bar", String)]
+-- ToMaybe '[ '("foo", Int), '("bar", String)] :: [(Symbol, *)]
+-- = '['("foo", Maybe Int), '("bar", Maybe [Char])]
 type family ToMaybe (xs :: [(Symbol, Type)]) where
   ToMaybe '[] = '[]
   ToMaybe ( '(s,t) ': xs ) = '(s, Maybe t) ': (ToMaybe xs)
 
+-- | Attributes cache is an infinite grid of cells where each cell is an
+-- extensible record. 'Memo' is a zipper into such a grid.
+--
+-- /Note/ how all pointers to neighbours are lazy!
 data Memo (attributes :: [(Symbol, Type)]) =
-  M { _mCurrent :: {-# UNPACK #-}!(AFieldRec (ToMaybe attributes))
+  M { _mCurrent :: {-# UNPACK #-}!(AFieldRec (ToMaybe attributes)) -- ^ Cached attributes
     , _mLeft    :: Memo attributes
     , _mRight   :: Memo attributes
     , _mUp      :: Memo attributes
     , _mDown    :: Memo attributes
     }
 
-
 -- | Returns the specified attribute.
-aget :: forall (name :: Symbol)
-               (field :: (Symbol, Type))
-               (x :: Type)
-               c
-               attributes
-               root.
-        ( KnownSymbol name
-        , NatToInt (RIndex field (ToMaybe attributes))
-        , field ~ '(name, Maybe x)
-        ) => Zipper c attributes root -> Maybe x
+aget
+  :: forall name field x c attributes root.
+     ( KnownSymbol name
+     , NatToInt (RIndex field (ToMaybe attributes))
+     , field ~ '(name, Maybe x)
+     )
+  => Zipper c attributes root
+  -> Maybe x
 aget (Z _ m _) = case Vinyl.aget @field (_mCurrent m) of (Field x) -> x
 
 -- | Updates the specified attribute.
-aput :: forall (name :: Symbol)
-               (field :: (Symbol, Type))
-               (x :: Type)
-               c
-               attributes
-               root.
-        ( KnownSymbol name
-        , NatToInt (RIndex field (ToMaybe attributes))
-        , field ~ '(name, Maybe x)
-        ) => x -> Zipper c attributes root -> Zipper c attributes root
+aput
+  :: forall name field x c attributes root.
+     ( KnownSymbol name
+     , NatToInt (RIndex field (ToMaybe attributes))
+     , field ~ '(name, Maybe x)
+     )
+  => x
+  -> Zipper c attributes root
+  -> Zipper c attributes root
 aput x z@(Z _ m _) = let f = _mCurrent m
                          f' = Vinyl.aput @field (Field (Just x)) f
                       in z { _zMemo = m { _mCurrent = f' } }
+
+-- | Given the @name@ of an attribute a monadic computation to calculate the
+-- attribute, returns a monadic computation of the same type which will use
+-- caching to speed-up the calculation.
+memoize
+  :: forall name field attributes x c root m.
+     ( KnownSymbol name
+     , NatToInt (RIndex field (ToMaybe attributes))
+     , field ~ '(name, Maybe x)
+     , Monad m
+     )
+  => StateT (Zipper c attributes root) m x
+  -> StateT (Zipper c attributes root) m x
+memoize semantic = (aget @name <$> get) >>= \case
+  Nothing -> do
+    -- NOTE: The following line is for debugging purposes only and should not
+    -- appear in production code
+    trace ("computing " <> symbolVal (Proxy @name)) (return ())
+    y <- semantic
+    modify (aput @name y)
+    return y
+  (Just y) -> return y
+
+-- | Specifies the position, where to evaluate the attribute.
+--
+-- /Note:/ using "Left" and "Right" is probably not a great idea. Better name
+-- suggestions are very welcome.
+data Direction = Left
+               | Right
+               | Parent
+               | Child {-# UNPACK #-}!Int
+               | Direction :~> Direction
+  deriving (Read, Show, Generic, NFData)
+
+unsafeAt :: Monad m => Direction -> StateT (Zipper c as root) m a -> StateT (Zipper c as root) m a
+unsafeAt d action = do
+  !(forth, back) <- mkMovements d <$> get
+  modify forth
+  x <- action
+  modify back
+  return x
+  where
+    mkMovements :: (z ~ Zipper c as root) => Direction -> z -> (z -> z, z -> z)
+    mkMovements !Left        _ = (unsafeLeft, unsafeRight)
+    mkMovements !Right       _ = (unsafeRight, unsafeLeft)
+    mkMovements !Parent (whichChild -> !n) = (unsafeUp, unsafeChild n)
+    mkMovements !(Child i)   _ = (unsafeChild i, unsafeUp)
+    mkMovements !(d1 :~> d2) z = let !(f1, b1) = mkMovements d1 z
+                                     !(f2, b2) = mkMovements d2 (f1 z)
+                                  in (f2 . f1, b1 . b2)
+
+{-
+class WithRollback z d where
+  unsafeAt :: Show a => d -> State z a -> State z a
+
+instance WithRollback (Zipper c attributes root) Direction where
+  unsafeAt direction action = case direction of
+    Left -> go unsafeLeft action unsafeRight
+    Right -> go unsafeRight action unsafeLeft
+    Parent -> do n <- whichChild <$> get
+                 modify unsafeUp
+                 x <- action
+                 modify (unsafeChild n)
+                 return x
+    Child i -> go (unsafeChild i) action unsafeUp
+    where
+      -- go :: (Zipper Cxt attributes root -> Zipper Cxt attributes root)
+      --    -> State (Zipper Cxt attributes root) a
+      --    -> (Zipper Cxt attributes root -> Zipper Cxt attributes root)
+      --    -> State (Zipper Cxt attributes root) a
+      go before now after = do
+        modify before
+        x <- now
+        modify after
+        return x
+-}
 
 -- | Context consists of a 'LocalContext' and a path to the @root@ of the
 -- zipper.
@@ -167,6 +295,11 @@ data Right c provides r where
 -- | A class of types that can can be dissected.
 class Dissectible (c :: Type -> Constraint) (a :: Type) where
   dissect :: a -> Left c a
+
+isRoot :: Monad m => StateT (Zipper c attributes root) m Bool
+isRoot = get >>= \case
+  (Z _ _ (_:>_))      -> return False
+  (Z _ _ RootContext) -> return True
 
 -- | Moves the 'Zipper' down.
 down :: forall c as root. Zipper c as root -> Maybe (Zipper c as root)
@@ -245,3 +378,21 @@ leave z = case top z of
           Nothing -> x
           Just x' -> top x'
 
+checkInvalidMove :: Maybe a -> a
+checkInvalidMove (Just x) = x
+checkInvalidMove Nothing  = error $! "Invalid move attempted!"
+
+unsafeUp :: Zipper c as root -> Zipper c as root
+unsafeUp = checkInvalidMove . up
+
+unsafeDown :: Zipper c as root -> Zipper c as root
+unsafeDown = checkInvalidMove . down
+
+unsafeLeft :: Zipper c as root -> Zipper c as root
+unsafeLeft = checkInvalidMove . left
+
+unsafeRight :: Zipper c as root -> Zipper c as root
+unsafeRight = checkInvalidMove . right
+
+unsafeChild :: Int -> Zipper c as root -> Zipper c as root
+unsafeChild !i = checkInvalidMove . child i
